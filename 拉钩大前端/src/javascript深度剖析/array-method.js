d0056dbd029b81e293745c82e0acd7f8e4222980
wangqiang <478536999@qq.com>
2022-05-07 09:54:00 +0800

    add

--- 拉钩大前端/src/javascript深度剖析/array-method.js
+++ 拉钩大前端/src/javascript深度剖析/array-method.js
@@ -1,3 +1,40 @@
 // map every some 高阶函数实现
 
 // map
+let arr = [1, 2, 3, 5, 7, 8];
+function map(array, fn) {
+  let results = [];
+  for (let item of array) {
+    results.push(fn(item));
+  }
+  return results;
+}
+
+// console.log(map(arr, (v) => v * v));
+
+// every
+function every(array, fn) {
+  let flag = false;
+  for (let item of array) {
+    flag = fn(item);
+    if (!flag) {
+      break;
+    }
+  }
+  return flag;
+}
+// console.log(every(arr, (v) => v > 3));
+
+// some
+function some(array, fn) {
+  let flag = false;
+  for (let item of array) {
+    flag = fn(item);
+    if (flag) {
+      break;
+    }
+  }
+  return flag;
+}
+
+console.log(some(arr, (v) => v > 3));
