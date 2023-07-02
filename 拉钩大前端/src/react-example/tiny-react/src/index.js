70d746ed017b9b511361e66e071f662af51148ad
wangqiang <478536999@qq.com>
2023-07-02 17:44:14 +0800

    add

--- 拉钩大前端/src/react-example/tiny-react/src/index.js
+++ 拉钩大前端/src/react-example/tiny-react/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import tinyReact from './TinyReact'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const virtualDOM = (
+  <div className="container">
+    <h1>你好 Tiny React</h1>
+    <h2 data-test="test">(编码必杀技)</h2>
+    <div>
+      嵌套1 <div>嵌套1.1</div>
+    </div>
+    <h3>(观察：这个将会被改变)</h3>
+    {2 == 1 && <div>如何2和1相等渲染当前内容</div>}
+    {2 == 2 && <div>2</div>}
+    <span>这是一段内容</span>
+    <button onClick={() => alert('你好')}>点击我</button>
+    <h3>这个将会被删除</h3>
+    2,3
+    <input type="text" value="13" />
+  </div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log('---virtualDOM,', virtualDOM)
