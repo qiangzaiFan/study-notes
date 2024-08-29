eb1452ce449d5b7d040760d69f0a7083bb275c17
wangqiang <478536999@qq.com>
2024-08-29 23:32:01 +0800

    add info

--- nanruiTest/nanrui/src/components/CascaderSelector.vue
+++ nanruiTest/nanrui/src/components/CascaderSelector.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
-    <a-cascader
-      :options="options"
-      v-model="selectedValue"
-      @change="onChange"
-      placeholder="请选择省/市/区"
-    />
-    <a-button @click="toggleModal" style="margin-top: 16px">修改数据</a-button>
-    <a-modal
-      v-model="isModalVisible"
-      title="修改数据"
-      @ok="handleOk"
-      @cancel="handleCancel"
-    >
+    <a-cascader :options="options" v-model="selectedValue" @change="onChange" placeholder="请选择省/市/区" />
+    <a-button @click="toggleModal('add')" style="margin-top: 16px">新增数据</a-button>
+    <a-button @click="toggleModal('edit')" style="margin-top: 16px">修改数据</a-button>
+    <a-modal v-model="isModalVisible" :title="modalTitle" @ok="handleOk" @cancel="handleCancel">
       <div>
-        <h4>新省</h4>
+        <h4>省</h4>
         <a-input v-model="newProvince" placeholder="输入省名" />
-        <h4>新市</h4>
+        <h4>市</h4>
         <a-input v-model="newCity" placeholder="输入市名" />
-        <h4>新区</h4>
+        <h4>区</h4>
         <a-input v-model="newDistrict" placeholder="输入区名" />
       </div>
     </a-modal>
@@ -34,7 +25,12 @@ export default {
       isModalVisible: false,
       newProvince: '',
       newCity: '',
-      newDistrict: ''
+      newDistrict: '',
+      selectedProvince: null,
+      selectedCity: null,
+      selectedDistrict: null,
+      modalType: 'add',
+      modalTitle: '新增数据'
     }
   },
   created() {
@@ -46,54 +42,116 @@ export default {
         console.log('---res,', res)
         this.options = res
       })
+
     // this.initializeOptions()
   },
   methods: {
+    //province  city  district
     // initializeOptions() {
     //   this.options = citiesData.map(province => ({
-    //     value: province.code,
-    //     label: province.name,
-    //     children: province.cities.map(city => ({
-    //       value: city.code,
-    //       label: city.name,
+    //     value: province.value,
+    //     label: province.label,
+    //     children: province.children.map(city => ({
+    //       value: city.value,
+    //       label: city.label,
     //       children: city.districts.map(district => ({
-    //         value: district.code,
-    //         label: district.name
+    //         value: district.value,
+    //         label: district.label
     //       }))
     //     }))
     //   }))
     // },
     onChange(value) {
-      console.log(value)
+      console.log('---onChange---value,', value)
+
+      this.selectedValue = value
+      this.getSelectedData(value)
     },
-    toggleModal() {
-      this.isModalVisible = !this.isModalVisible
+    getSelectedData(value) {
+      this.selectedProvince = this.options.find(p => p.value === value[0])
+      if (this.selectedProvince) {
+        this.selectedCity = this.selectedProvince.children.find(c => c.value === value[1])
+      }
+    },
+    toggleModal(type) {
+      this.resetInputs()
+      debugger
+      if (type === 'edit' && this.selectedValue.length === 0) {
+        this.$message.info('请选择省市区级联选择器！')
+        return
+      }
+      // 如果已选择省市区，则填充输入框
+      this.modalType = type
+      this.modalTitle = type === 'add' ? '新增数据' : '修改数据'
+      if (type === 'edit') {
+        this.newProvince = this.selectedValue[0]
+        this.newCity = this.selectedValue[1]
+        this.newDistrict = this.selectedValue[2]
+      }
+      this.isModalVisible = true
     },
     handleOk() {
       if (this.newProvince) {
-        const province = this.options.find(p => p.label === this.newProvince)
-        if (!province) {
-          // 如果省不存在，创建新的省
+        const provinceIndex = this.options.findIndex(p => p.label === this.newProvince)
+        if (provinceIndex === -1) {
+          // 添加新省
           const newProvinceData = {
             label: this.newProvince,
             value: `00${this.options.length + 1}`,
             children: []
           }
           this.options.push(newProvinceData)
+          this.selectedProvince = newProvinceData
+        } else {
+          // 修改省
+          this.selectedProvince = this.options[provinceIndex]
         }
-        this.clearInputs()
+        if (this.newCity) {
+          const cityIndex = this.selectedProvince.children.findIndex(c => c.label === this.newCity)
+          if (cityIndex === -1) {
+            // 添加新市
+            const newCityData = {
+              label: this.newCity,
+              value: `00${this.selectedProvince.children.length + 1}`,
+              districts: []
+            }
+            this.selectedProvince.children.push(newCityData)
+            this.selectedCity = newCityData
+          } else {
+            // 修改市
+            this.selectedCity = this.selectedProvince.children[cityIndex]
+          }
+
+          if (this.newDistrict) {
+            const districtIndex = this.selectedCity.districts.findIndex(d => d.label === this.newDistrict)
+            if (districtIndex === -1) {
+              // 添加新区
+              const newDistrictData = {
+                label: this.newDistrict,
+                value: `00${this.selectedCity.districts.length + 1}`
+              }
+              this.selectedCity.districts.push(newDistrictData)
+            } else {
+              // 修改区名
+              this.selectedCity.districts[districtIndex].label = this.newDistrict
+            }
+          }
+        }
+
         // this.initializeOptions()
       }
-      this.toggleModal()
+      this.handleCancel()
     },
     handleCancel() {
-      this.clearInputs()
-      this.toggleModal()
+      this.resetInputs()
+      this.isModalVisible = false
     },
-    clearInputs() {
+    resetInputs() {
       this.newProvince = ''
       this.newCity = ''
       this.newDistrict = ''
+      this.selectedProvince = null
+      this.selectedCity = null
     }
   }
 }
