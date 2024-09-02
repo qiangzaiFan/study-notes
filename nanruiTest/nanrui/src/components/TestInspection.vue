86884dac30fb4d82ca3a627cf93a867c35e51251
wangqiang <wangqiang@git.com.cn>
2024-09-02 19:11:05 +0800

    add

--- nanruiTest/nanrui/src/components/TestInspection.vue
+++ nanruiTest/nanrui/src/components/TestInspection.vue
@@ -8,8 +8,92 @@
       @onCancel="handleCancel"
     >
       <a-form :form="form" :modal="modalData">
-        <a-form-item label="检修单名称"> </a-form-item>
+        <a-form-item label="检修单名称">
+          <a-input
+            v-modal="modalData.title"
+            v-decorator="[
+              'title',
+              {
+                rules: [{ required: true, message: '检修单名称' }]
+              }
+            ]"
+          ></a-input>
+        </a-form-item>
+        <a-form-item label="姓名">
+          <a-input
+            v-modal="modalData.title"
+            v-decorator="[
+              'name',
+              {
+                rules: [{ required: true, message: '姓名' }]
+              }
+            ]"
+          ></a-input>
+        </a-form-item>
+        <a-form-item label="手机号码">
+          <a-input
+            v-modal="modalData.title"
+            v-decorator="[
+              'name',
+              {
+                rules: [
+                  { required: true, message: '手机号码' },
+                  {
+                    validate: /1[3456789]\d{9}/
+                  }
+                ]
+              }
+            ]"
+          ></a-input>
+        </a-form-item>
+        <a-form-item label="检修附件">
+          <a-upload
+            :fileList="modalData.fileList"
+            name="file"
+            action="http://localhost:8080/upload"
+            :multiple="true"
+            @onChange="changeFile"
+            @remove="removeFile"
+            @preview="handlePreview"
+            v-decorator="[
+              'fileList',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: '请上传附件'
+                  }
+                ]
+              }
+            ]"
+          >
+            <a-button>上传附件</a-button>
+          </a-upload>
+        </a-form-item>
       </a-form>
     </a-modal>
+    <a-table :data-source="dataSource" :columns="columns">
+      <template slot="fileList" slot-scope="text, record, index">
+        <span v-for="(imgItem, imgIndex) in record.fileList" :key="imgIndex">
+          <img src="{{imgItem}}" alt="" />
+        </span>
+      </template>
+    </a-table>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      dataSource: [],
+      columns: [
+        {
+          title: '附件',
+          dataIndex: 'fileList',
+          scopedSlot: { customRender: 'file' }
+        }
+      ]
+    }
+  }
+}
+</script>
