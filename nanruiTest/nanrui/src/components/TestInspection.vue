ce47458994d5e8aee4bc73cb7e7518e7fe5f8b88
wangqiang <wangqiang@git.com.cn>
2024-09-02 17:41:36 +0800

    add

---
+++ nanruiTest/nanrui/src/components/TestInspection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a-button @click="handleAdd">新建</a-button>
+    <a-modal
+      :modal="showModalVisible"
+      title="检修单"
+      @onOK="handleOk"
+      @onCancel="handleCancel"
+    >
+      <a-form :form="form" :modal="modalData">
+        <a-form-item label="检修单名称"> </a-form-item>
+      </a-form>
+    </a-modal>
+  </div>
+</template>
